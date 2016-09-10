7b45555063a8e1d271d14da70386f78bd2bb21c0
Eli Stone <eli.stonium@gmail.com>
2016-09-09 19:38:39 -0600

    Add CMakeLists, add build.sh for easy building, remove makefile. Use cmake -DCMAKE_BUILD_TYPE=DEBUG for automatic debug flags when building

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# This program will automatically build life with normal defaults
+#
+# Please be sure to install sfml and cmake. The readme should explain everything.
+#
+
+#Do not move build.sh. You can execute it from anywhere though.
+cd $(dirname $0)
+
+SRC_PATH=$(pwd)
+BUILD_PATH="$SRC_PATH/build"
+EXE_NAME="openlife"
+
+mkdir "$BUILD_PATH"
+cd "$BUILD_PATH"
+
+#TODO: add any possible cmake variables here
+cmake "$SRC_PATH" -DCMAKE_BUILD_TYPE=DEBUG
+
+make -j $(nproc)
+chmod a+x "$EXE_NAME"
+cp "$EXE_NAME" ..
