7cacd112d34e04c50e977232f2fd227b7a164b33
LSaldyt <lucassaldyt@yahoo.com>
2016-09-30 20:00:36 -0600

    Updates to window resizing and views
    Text and images will not stretch when window is resized!
    
    However, info view will not scale when scrolling, which is the desired
    behavior

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     using objects::Board;
     Board board;
-    board.run(80); // Initial simulation with 80 starting cells
+    board.run(80, 2000, 2000); // Initial simulation with 80 starting cells
 
     return 0;
 }
