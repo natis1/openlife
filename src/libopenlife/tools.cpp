d737497a29a4d25551d9b742221213080a6aeda4
LSaldyt <lucassaldyt@gmail.com>
2017-02-09 17:44:00 -0700

    Parameter tweaks and angle code refactor

--- src/libopenlife/tools.cpp
+++ src/libopenlife/tools.cpp
@@ -69,19 +69,22 @@ void writeFile(std::vector<std::string> content, std::string filename)
 
 double getLimitedAngle(double ideal, double current, double limit)
 {
-    auto opposite = modAngle(current + 180.);
-    auto diff = angleDiff(ideal, current);
-    if (diff < limit)
+    auto ideal_diff = angleDiff(ideal, current);
+
+    auto clock_diff  = angleDiff(ideal, modAngle(current - limit));
+    auto cclock_diff = angleDiff(ideal, modAngle(current + limit));
+
+    if (ideal_diff < limit)
     {
         return ideal;
     }
-    else if (ideal > current and ideal < opposite)
+    else if (clock_diff  < cclock_diff) // Turning clockwise is less distance by angle
     {
-        return current + limit;
+        return current - limit;
     }
     else
     {
-        return current - limit;
+        return current + limit;
     }
 }
 
